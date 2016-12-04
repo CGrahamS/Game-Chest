22edf617b37ea11643b5bed9b43e1e4d1f27c4b7
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-12-04 15:53:06 -0800

    add game listadapter and viewholder

--- app/src/main/java/com/epicodus/gamechest/adapters/GameListAdapter.java
+++ app/src/main/java/com/epicodus/gamechest/adapters/GameListAdapter.java
@@ -1,7 +1,67 @@
 package com.epicodus.gamechest.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.epicodus.gamechest.R;
+import com.epicodus.gamechest.models.Game;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by CGrahamS on 12/4/16.
  */
-public class GameListAdapter {
+public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.GameViewHolder> {
+    private ArrayList<Game> mGames = new ArrayList<>();
+    private Context mContext;
+
+    public GameListAdapter(Context context, ArrayList<Game> games) {
+        mContext = context;
+        mGames = games;
+    }
+
+    @Override
+    public GameListAdapter.GameViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_list_item, parent, false);
+        GameViewHolder viewHolder = new GameViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(GameListAdapter.GameViewHolder holder, int position) {
+        holder.bindGame(mGames.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mGames.size();
+    }
+
+    public class GameViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.gameNameTextView) TextView mGameNameTextView;
+        @Bind(R.id.gameImageView) ImageView mGameImageView;
+        @Bind(R.id.gameReleaseDateTextView) TextView mGameReleaseDateTextView;
+
+        private Context mContext;
+
+        public GameViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindGame(Game game) {
+            mGameNameTextView.setText(game.getName());
+            mGameReleaseDateTextView.setText(game.getReleaseDate());
+        }
+
+    }
 }

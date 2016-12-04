15c9ae235ab37822b2bddb6843a092b0aa21ddf9
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-12-03 18:14:41 -0800

    add initial game list item design

--- app/src/main/java/com/epicodus/gamechest/MainActivity.java
+++ app/src/main/java/com/epicodus/gamechest/MainActivity.java
@@ -2,12 +2,15 @@ package com.epicodus.gamechest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }

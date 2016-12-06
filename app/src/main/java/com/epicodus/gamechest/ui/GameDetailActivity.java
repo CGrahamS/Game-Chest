2715731fab3bed7c9632f8a92af37aae4c95d254
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-12-05 22:31:43 -0800

    add GameDetailActivity

---
+++ app/src/main/java/com/epicodus/gamechest/ui/GameDetailActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.gamechest.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.gamechest.R;
+
+public class GameDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_detail);
+    }
+}

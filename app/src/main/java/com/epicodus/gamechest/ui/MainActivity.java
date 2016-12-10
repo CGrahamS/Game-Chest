0790fca0a6a093c63fd1e1cfe7478c840bba5e35
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-12-10 10:30:39 -0800

    fix pager adapter to navigate to clicked item in pager adapter

--- app/src/main/java/com/epicodus/gamechest/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/gamechest/ui/MainActivity.java
@@ -1,19 +1,12 @@
 package com.epicodus.gamechest.ui;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.epicodus.gamechest.Constants;
 import com.epicodus.gamechest.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -60,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
         if (v == mFavoritedGamesButton) {
-            Intent intent = new Intent(MainActivity.this, SavedGameListActivity.class);
+            Intent intent = new Intent(MainActivity.this, FavoriteGamesListActivity.class);
             startActivity(intent);
         }
     }

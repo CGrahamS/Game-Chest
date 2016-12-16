a75328089bcc109eccc06d1591b5a076443cf81e
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-12-16 15:17:40 -0800

    attach gestures to firebase

--- app/src/main/java/com/epicodus/gamechest/Constants.java
+++ app/src/main/java/com/epicodus/gamechest/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String GIANT_BOMB_BASE_URL = "http://www.giantbomb.com/api/";
     public static final String PREFERENCES_GAME_KEY = "game";
     public static final String FIREBASE_CHILD_USERS = "users";
+    public static final String FIREBASE_QUERY_INDEX = "index";
 }
